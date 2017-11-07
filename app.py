e6c58e8e6cff748647db11b4ac65dcab557eb10b
karthiarjun <arjunkarthik1991@gmail.com>
2017-11-07 20:17:48 +0530

    REQ

--- app.py
+++ app.py
@@ -33,7 +33,11 @@ def train():
 		result= out[0]
 		#print(result)
 		#return result
-		res = json.dumps({"speech": result,"displayText": result,"source": "apiai-zmlroles-train-sample"}, indent=4)# print(res)
+		res = json.dumps({"speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": {},
+"contextOut": [],
+"source": "DuckDuckGo"})# print(res)
 		r = make_response(res)
 		r.headers['Content-Type'] = 'application/json'
 		return r
